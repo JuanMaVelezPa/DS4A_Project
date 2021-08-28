0b0df1c56879e3440e6a76150246c74ec3154dfc
David Rubio Méndez <dafrubiome@unal.edu.co>
2021-08-28 00:47:14 -0500

    add labels

--- indicators.py
+++ indicators.py
@@ -212,7 +212,8 @@ def update_graph(value1,value2,value3,start_date,end_date):
     
     
     sales1 = sales_prod.groupby(['ANIO','MES'])['TOTAL'].sum().to_frame().reset_index()
-    fig = px.line(sales1, x="MES", y="TOTAL", color='ANIO')
+    fig = px.line(sales1, x="MES", y="TOTAL", color='ANIO', labels = {1:"JAN",
+    2:"FEB",3:"MAR",4:"APR",5:"MAY",6:"JUN",7:"JUL",8:"AUG",9:"SEP",10:"OCT",11:"NOV",12:"DEC"})
     
     return fig
 
