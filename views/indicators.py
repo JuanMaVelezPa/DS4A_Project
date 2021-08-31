48506155006302b720bac867813eab4ced6d3f7f
David Rubio Méndez <dafrubiome@unal.edu.co>
2021-08-30 21:21:23 -0500

    dup

--- views/indicators.py
+++ views/indicators.py
@@ -289,43 +289,6 @@ def update_graph(value1,value2,value3,start_date,end_date):
     
     return fig
 
-## Colores más vendidos
-@app.callback(
-    Output('graph_general_4', 'figure'),
-    [Input('dropdown_category', 'value'),
-     Input('dropdown_subcategory', 'value'),
-     Input('dropdown_tienda', 'value'),
-     Input('calendar', 'start_date'),
-     Input('calendar', 'end_date')])
-
-def update_graph(value1,value2,value3,start_date,end_date):
-    if(value3 == []):
-        temp = DataManager().sales_prod
-    else:
-        temp = DataManager().sales_prod.query("TIENDA==@value3")
-    if (value1 == [] and value2 == []):
-        sales_prod = temp
-    elif (value1 != [] and value2 == []):
-        sales_prod = temp.query("CATEGORIA==@value1")
-    elif (value1 == [] and value2 != []):
-        sales_prod = temp.query("SUBCATEGORIA==@value2")
-    else:
-        sales_prod = temp.query("CATEGORIA==@value1")
-        sales_prod = temp.query("SUBCATEGORIA==@value2")
-    mask = (sales_prod['FECHA'] >= start_date) & (sales_prod['FECHA'] <= end_date)
-    sales_prod = sales_prod.loc[mask]
-    
-    df = sales_prod.groupby(['COLOR_POS'])['CANTIDAD'].sum()
-    colors = ["#FFFF00","#0000FF","#e4e4a1","#FFFFFF","#A52A2A", "#9999ff","#808080","#FFA500","#000000", 
-    "#0f3c14", "#ff0000" ]
-    
-    fig = go.Figure(data=[go.Bar(
-        x= df.index,
-        y= df,
-        marker_color=colors, # marker color can be a single color value or an iterable
-    )])
-    
-    return fig
 
 @app.callback(
     Output('graph_features_1', 'figure'),
